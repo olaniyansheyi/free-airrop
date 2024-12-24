5590b8749ed3444d088a954b85584a257d6eb6ad
sheymantech <olaniyansheyi1704@gmail.com>
2024-12-24 16:14:13 +0100

    new

--- lib/wallet-modal.jsx
+++ lib/wallet-modal.jsx
@@ -1,13 +1,26 @@
 "use client";
 
+import React, { useState } from "react";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount } from "wagmi";
 
 export default function ConnectButton() {
   const { open } = useWeb3Modal();
+  const { isConnected } = useAccount(); // Check if the user is connected
+  const [buttonText, setButtonText] = useState("Connect Wallet");
+
+  // Update button text dynamically based on connection status
+  React.useEffect(() => {
+    if (isConnected) {
+      setButtonText("Connected, Claim Token Now!");
+    } else {
+      setButtonText("Connect Wallet");
+    }
+  }, [isConnected]);
 
   return (
     <button className="connect-button" onClick={() => open()}>
-      Connect Wallet
+      {buttonText}
     </button>
   );
 }
